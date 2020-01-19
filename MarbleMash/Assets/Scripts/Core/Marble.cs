807ebc94d76ac4f8944a567af924a37f5540a921
Thomas Tran <tran.th.thomas@gmail.com>
2020-01-18 23:11:04 -0500

    Extend Marble class to allow marbles to move

--- MarbleMash/Assets/Scripts/Core/Marble.cs
+++ MarbleMash/Assets/Scripts/Core/Marble.cs
@@ -7,6 +7,8 @@ public class Marble : MonoBehaviour
     public int xIndex;
     public int yIndex;
 
+    bool m_isMoving = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +18,57 @@ public class Marble : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move((int) transform.position.x + 1, (int) transform.position.y, 0.5f);
+        }
 
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move((int) transform.position.x - 1, (int) transform.position.y, 0.5f);
+        }
     }
 
     public void SetCoordinates(int x, int y)
     {
         xIndex = x;
         yIndex = y;
+    }
+
+    public void Move(int destX, int destY, float timeToMove)
+    {
+        if (!m_isMoving)
+        {
+            StartCoroutine(MoveRoutine(new Vector3(destX, destY, 0), timeToMove));
+        }
+    }
+
+    IEnumerator MoveRoutine(Vector3 destination, float timeToMove)
+    {
+        Vector3 startPosition = transform.position;
+        bool reachedDestination = false;
+        float elapsedTime = 0f;
+
+        m_isMoving = true;
+
+        while (!reachedDestination)
+        {
+            if (Vector3.Distance(transform.position, destination) < 0.01f)
+            {
+                reachedDestination = true;
+                transform.position = destination;
+                SetCoordinates((int) destination.x, (int) destination.y);
+                break;
+            }
+
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp(elapsedTime / timeToMove, 0f, 1f);
+            transform.position = Vector3.Lerp(startPosition, destination, t);
+
+            // Wait until next frame
+            yield return null;
+        }
+
+        m_isMoving = false;
     }
 }
